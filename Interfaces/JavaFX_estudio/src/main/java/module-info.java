26a3b3c6dcce5757da057a911cd59c06528b0c1d
Barshal <hector_mur@hotmail.es>
2026-02-16 16:48:32 +0100

    Cambios

--- Interfaces/JavaFX_estudio/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.javafx_estudio {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.javafx_estudio to javafx.fxml;
-    exports org.example.javafx_estudio;
-}
